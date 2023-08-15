b557c8bb267fa85f74c20e57ad3b9000bf587a21
Sine <ssnngcongo@gmail.com>
2023-08-14 19:56:14 -0700

    updated script

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     multiples = []
     for i in range(len(my_list)):
-        if my_list[i] %2 == 0:
+        if my_list[i] % 2 == 0:
             multiples.append(True)
         else:
             multiples.append(False)

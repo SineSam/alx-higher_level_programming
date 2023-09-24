940e7450b76e9617c4d8bac450ae7f166ca5aac0
Sine <ssnngcongo@gmail.com>
2023-09-24 09:53:23 -0700

    created script that defines a square and validates the values of the size

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Define class Square"""
+
+class Square:
+    """This defines the Square class
+
+    This class has no public attributes
+
+    """
+    def __init__(self, size=0):
+        """
+        This method initiates the Square
+
+        Args:
+        size (int): defines the size of the square
+        Validated with try/except
+
+        """
+        try:
+            self.__size = size
+            if size < 0:
+                raise ValueError
+            if type(size) is not int:
+                raise TypeError
+        except TypeError:
+            raise TypeError("size must be an integer")
+        except ValueError:
+            raise ValueError("size must be >= 0")

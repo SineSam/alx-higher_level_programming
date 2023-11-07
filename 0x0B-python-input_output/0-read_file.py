4e6c52ad59d182778ce88d71b884728431e95fb3
Sine <ssnngcongo@gmail.com>
2023-11-07 12:41:00 -0800

    updated the read file script

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,4 +4,4 @@ def read_file(filename=""):
     """Reads text file and prints it out to stdout"""
     with open(filename, mode="r", encoding="utf-8") as f:
         content = f.read()
-        print(content)
+        print(content, end="")

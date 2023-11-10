6fbf7e6bb21fdab9171105b295ed7b8a253aa5de
Sine <ssnngcongo@gmail.com>
2023-11-10 13:11:58 -0800

    updated script

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Define a class Square"""
 
+
 class Square:
     """Attributes of a square"""
 
     def __init__(self, size):
         """Initialise a square
-        
         Args:
             size (int): size of square.
         """

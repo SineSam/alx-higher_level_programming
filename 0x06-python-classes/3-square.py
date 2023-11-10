f8a1db462f0b47549ee13f9ce5df5d97b2da2b4c
Sine <ssnngcongo@gmail.com>
2023-11-10 12:20:03 -0800

    created script to calculate area

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Define class Square"""
+
+
+class Square:
+    def __init__(self, size=0):
+        """
+        This method initiates the Square
+
+        Args:
+        size (int): defines the size of the square
+        Validated with try/except
+
+        """
+        try:
+            self.__size = size
+            if size < 0:
+                raise ValueError
+            if type(size) is not int:
+                raise TypeError
+        except TypeError:
+            raise TypeError("size must be an integer")
+        except ValueError:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return (self.__size ** 2)

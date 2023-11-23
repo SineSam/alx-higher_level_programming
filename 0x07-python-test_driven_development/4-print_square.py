565e782ddd41610b6f46d9347fd59dad1c8a68fc
Sine <ssnngcongo@gmail.com>
2023-11-23 12:38:02 -0800

    created test for printing square module

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/pythin3
+# 4-print_square.py
+"""This module contains a method that prints a square"""
+
+
+def print_square(size):
+    """ Method that prints a square with the '#' character
+
+    Args:
+    size (integer): Length of a square
+
+    raises:
+    TypeError: size is not an integer
+    ValueError: size is less than zero
+    """
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    print(("#" * size + "\n") * size, end="")
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/4-print_square.txt")

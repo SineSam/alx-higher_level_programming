3077f2bf99f021bd51e70b0cadc0c96e1f175abf
Sine <ssnngcongo@gmail.com>
2023-11-25 01:09:32 -0800

    created script that checks if object is instance of class

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# 2-is_same_class.py
+
+def is_same_class(obj, a_class):
+    """returns True if the object is exactly an instance of the specified class
+    otherwise False
+
+    Args:
+    obj: The object to be tested
+    a_class: The class
+    """
+    return (type(obj) == a_class)

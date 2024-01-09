d7809b6adae0c3ef2347cfbbd671622e313fdbcd
SineSam <ssnngcongo@gmail.com>
2024-01-09 00:34:08 -0800

    updated script to be semistandard compliant

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
 const i = process.argv[2];
 if (isNaN(i)) {
-  console.log('Missing size')
+  console.log('Missing size');
 } else {
   for (let x = 0; x < i; x++) {
-	  console.log('X'.repeat(i));
+    console.log('X'.repeat(i));
   }
 }
-  

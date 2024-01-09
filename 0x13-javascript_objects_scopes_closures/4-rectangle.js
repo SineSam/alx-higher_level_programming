3394e6235b0636905ad36db19e74ba1dbc123384
SineSam <ssnngcongo@gmail.com>
2024-01-09 14:58:08 -0800

    updated script to be semistandard compliant

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,11 +1,12 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w >  0 && h > 0) {
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
+
   print () {
     for (let l = 0; l < this.height; l++) {
       let r = '';
@@ -17,15 +18,14 @@ class Rectangle {
   }
 
   rotate () {
-    let tmp = this.width;
+    const tmp = this.width;
     this.width = this.height;
     this.height = tmp;
   }
-  
+
   double () {
     this.width *= 2;
     this.height *= 2;
   }
-
 }
 module.exports = Rectangle;
